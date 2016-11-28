c67e312a889937ad7344a99a25f94b548181ff50
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-11-28 17:30:21 -0600

    checkpoint 22

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,58 +1,59 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+    def show
+      @post = Post.find(params[:id])
+    end
 
-  def new
-    @post = Post.new
-  end
+    def new
+     @topic = Topic.find(params[:topic_id])
+      @post = Post.new
+    end
 
-  def create
-     @post = Post.new
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
+   def create
+      @post = Post.new
+      @post.title = params[:post][:title]
+      @post.body = params[:post][:body]
+     @topic = Topic.find(params[:topic_id])
+     @post.topic = @topic
 
-     if @post.save
-       flash[:notice] = "Post was saved."
-       redirect_to @post
-     else
+      if @post.save
 
-       flash.now[:alert] = "There was an error saving the post. Please try again."
-       render :new
+        flash[:notice] = "Post was saved."
+        redirect_to [@topic, @post]
+      else
+        flash.now[:alert] = "There was an error saving the post. Please try again."
+        render :new
      end
    end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+   def edit
+     @post = Post.find(params[:id])
+   end
 
-  def update
+   def update
      @post = Post.find(params[:id])
      @post.title = params[:post][:title]
      @post.body = params[:post][:body]
 
-     if @post.save
-       flash[:notice] = "Post was updated."
-       redirect_to @post
-     else
-       flash.now[:alert] = "There was an error saving the post. Please try again."
-       render :edit
+      if @post.save
+        flash[:notice] = "Post was updated."
+        redirect_to [@post.topic, @post]
+      else
+        flash.now[:alert] = "There was an error saving the post. Please try again."
+        render :edit
      end
    end
 
    def destroy
      @post = Post.find(params[:id])
 
-     if @post.destroy
-       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to posts_path
-     else
-       flash.now[:alert] = "There was an error deleting the post."
-       render :show
+      if @post.destroy
+        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+        redirect_to @post.topic
+      else
+        flash.now[:alert] = "There was an error deleting the post."
+        render :show
      end
    end
-end
+
+ end
