51e631e80c736bb2a881b1f64ca55f9b4303c527
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-12-06 02:37:39 -0600

    assignment 28

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -63,7 +63,7 @@ class TopicsController < ApplicationController
    end
 
    def authorize_user
-     unless current_user.admin?
+      unless (current_user.moderator? && params[:action] == 'edit') || current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path
      end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
+
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
 
@@ -13,5 +14,5 @@ class User < ActiveRecord::Base
 
    has_secure_password
 
-   enum role: [:member, :admin]
+   enum role: [:member, :admin, :moderator]
 end
