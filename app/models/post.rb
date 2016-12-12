ebbb84a36dbf3e17ed1c2806844ccda84250c67b
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-12-12 02:22:37 -0600

    checkpoint 32

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,13 @@
 class Post < ActiveRecord::Base
-  belongs_to :topic
-  belongs_to :user
-  has_many :comments, dependent: :destroy
-  has_many :votes, dependent: :destroy
-  has_many :favorites, dependent: :destroy
+   belongs_to :topic
+   belongs_to :user
+   has_many :comments, dependent: :destroy
+   has_many :votes, dependent: :destroy
+   has_many :favorites, dependent: :destroy
 
-  default_scope { order('rank DESC') }
+   default_scope { order('rank DESC') }
+
+   scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
 
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
